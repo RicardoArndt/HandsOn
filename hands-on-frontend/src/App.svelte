77d51eca99acad53acee0691af0160a8f80e2ec8
RicardoArndt <ricardoguilhermearndt@gmail.com>
2022-05-30 21:16:36 -0300

    Estrutura inicial + Header

---
+++ hands-on-frontend/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { Router, Route } from "svelte-navigator";
+	import Header from "./@components/Header.svelte";
+</script>
+
+<Router>
+	<main>
+		<Header />
+
+		<Route path="/">
+			<section class="content">
+				<h1>Home Page</h1>
+			</section>
+		</Route>
+
+		<Route path="/posts">
+			<section class="content">
+				<h1>Posts Page</h1>
+			</section>
+		</Route>
+
+		<Route path="/users">
+			<section class="content">
+				<h1>Users Page</h1>
+			</section>
+		</Route>
+	</main>
+</Router>
+
+<style>
+	.content {
+		width: 80%;
+		margin-left: 207px;
+		margin-top: 25px;
+    margin-right: 50px;
+    padding-left: 15px;
+    padding-right: 15px;
+	}
+</style>
