a14dfc753ae0c2bded6abb1b2d1c0bfeee8f6456
RicardoArndt <ricardoguilhermearndt@gmail.com>
2022-07-05 11:52:05 -0300

    try catch publication endpoints

--- hands-on-backend/src/routes/publication.ts
+++ hands-on-backend/src/routes/publication.ts
@@ -8,35 +8,54 @@ const service = injector.getPublicationService();
 
 publicationRouter.post('/', (req, res) => {
   const { body } = req;
-
-  service.insert(body as IPublicationCreateRequest).then(publicationId => {
-    res.send(publicationId);
-  });
+  try {
+    service.insert(body as IPublicationCreateRequest).then(publicationId => {
+      res.send(publicationId);
+    });
+  } catch (err) {
+    res.status(500);
+    res.send(err.message);
+  }
 });
 
 publicationRouter.put('/:id', (req, res) => {
   const { id } = req.params;
   const { body } = req;
 
-  service.update(id, body as IPublicationCreateRequest).then(() => {
-    res.send();
-  });
+  try {
+    service.update(id, body as IPublicationCreateRequest).then(() => {
+      res.send();
+    });
+  } catch (err) {
+    res.status(500);
+    res.send(err.message);
+  }
 });
 
 publicationRouter.get('/', (req, res) => {
   const { title } = req.query;
 
-  service.getAll(title as string).then(publications => {
-    res.send(publications);
-  });
+  try {
+    service.getAll(title as string).then(publications => {
+      res.send(publications);
+    });
+  } catch (err) {
+    res.status(500);
+    res.send(err.message);
+  }
 });
 
 publicationRouter.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  service.getById(id).then(publication => {
-    res.send(publication);
-  });
+  try {
+    service.getById(id).then(publication => {
+      res.send(publication);
+    });
+  } catch (err) {
+    res.status(500);
+    res.send(err.message);
+  }
 });
 
 export default publicationRouter;
